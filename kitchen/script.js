1c1938b0dd066dfffa475053a4466570e1e50f24
Vojtěch Týnavský <vojtech.tynavsky@seznam.cz>
2024-01-25 19:24:27 +0100

    Not complete

---
+++ kitchen/script.js
@@ -0,0 +1,33 @@
+const button = document.querySelector('.btn');
+const emptyBox = document.querySelector('.empty');
+const timerSecond = document.getElementById('second');
+const timerMinute = document.getElementById('minutes');
+let intervalId;
+let totalTime = 60;
+button.addEventListener('click', () => {
+    let additionalMinutes = Number(timerMinute.value) || 0;
+    let additionalSeconds = Number(timerSecond.value) || 0;
+    if (additionalMinutes < 0 || additionalSeconds < 0) {
+        alert('Time is over!');
+        return;
+    }
+    totalTime += additionalMinutes * 60 + additionalSeconds;
+    clearInterval(intervalId);
+    displayTime(totalTime);
+    const countDown = () => {
+        intervalId = setInterval(() => {
+            totalTime--;
+            displayTime(totalTime);
+            if (totalTime <= 0) {
+                clearInterval(intervalId);
+                alert('Time is over');
+            }
+        }, 1000);
+    };
+    countDown();
+});
+function displayTime(totalTime) {
+    let displayMinutes = Math.floor(totalTime / 60);
+    let displaySeconds = totalTime % 60;
+    emptyBox.innerHTML = `${displayMinutes}:${displaySeconds}`;
+}
